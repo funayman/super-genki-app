feec66183beee9307cd15c82d93a04dc460d16c7
Dave Derderian <dave@drt.sh>
2019-02-13 15:55:58 +0900

    search implemented closes #3 fixes #4

--- app/src/main/java/io/royaloaklabs/supergenki/domain/JapaneseCard.java
+++ app/src/main/java/io/royaloaklabs/supergenki/domain/JapaneseCard.java
@@ -1,5 +1,10 @@
 package io.royaloaklabs.supergenki.domain;
 
+import android.database.Cursor;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class JapaneseCard {
     public String japaneseText;
     public String englishMeaning;
@@ -23,6 +28,42 @@ public class JapaneseCard {
 
     public void setPhotoId(String photoId) {
         this.photoId = photoId;
+    }
+
+
+    public static List<JapaneseCard> buildListFromCursor(Cursor cursor) {
+      List<JapaneseCard> mCards = new ArrayList<JapaneseCard>();
+
+      for (; cursor.moveToNext(); ) {
+        JapaneseCard jpCard = new JapaneseCard();
+
+        String kanjiCol = cursor.getString(cursor.getColumnIndex("kanji"));
+        String kanaCol = cursor.getString(cursor.getColumnIndex("kana"));
+        String senseCol = cursor.getString(cursor.getColumnIndex("gloss"));
+
+        String[] kanji = kanjiCol.split(";");
+        String[] kana = kanaCol.split(";");
+        String[] sense = senseCol.split("\\|\\|");
+
+        if (kanji.length != 0 && !kanji[0].isEmpty()) {
+          jpCard.setJapaneseText(kanji[0]);
+        } else {
+          jpCard.setJapaneseText(kana[0]);
+        }
+
+        String[] senseArr = sense[0].split(";;");
+        StringBuilder sbSense = new StringBuilder();
+        sbSense.append(senseArr[0]);
+        for (int i = 1; i < senseArr.length; i++) {
+          sbSense.append("; ");
+          sbSense.append(senseArr[i]);
+        }
+        jpCard.setEnglishMeaning(sbSense.toString());
+
+        mCards.add(jpCard);
+      }
+
+      return mCards;
     }
 
 
