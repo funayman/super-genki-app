f19db08767fec33a8aecdf4994f7d5cae54796cb
Dave Derderian <dave@drt.sh>
2019-02-13 13:38:36 +0900

    reformatting

--- app/src/main/java/io/royaloaklabs/supergenki/MainActivity.java
+++ app/src/main/java/io/royaloaklabs/supergenki/MainActivity.java
@@ -16,58 +16,58 @@ import io.royaloaklabs.supergenki.database.DictionaryAdapter;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView mTextMessage;
-    private RecyclerView rv;
-    private RecyclerView.Adapter mAdapter;
-    private RecyclerView.LayoutManager mLayoutManager;
+  private TextView mTextMessage;
+  private RecyclerView rv;
+  private RecyclerView.Adapter mAdapter;
+  private RecyclerView.LayoutManager mLayoutManager;
 
-    public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.navigation, menu);
+  public boolean onCreateOptionsMenu(Menu menu) {
+    MenuInflater inflater = getMenuInflater();
+    inflater.inflate(R.menu.navigation, menu);
 
-        final MenuItem searchMenuItem = menu.findItem( R.id.search);
-        final SearchView searchView   = (SearchView) searchMenuItem.getActionView();
-        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
-            @Override
-            public boolean onQueryTextSubmit(String query) {
-                Toast toast = Toast.makeText(getApplicationContext(), "Search For="+query, Toast.LENGTH_SHORT);
-                toast.show();
-                if( ! searchView.isIconified()) {
-                    searchView.setIconified(true);
-                }
-                searchMenuItem.collapseActionView();
-                return false;
-            }
-            @Override
-            public boolean onQueryTextChange(String s) {
-                // UserFeedback.show( "SearchOnQueryTextChanged: " + s);
-                return false;
-            }
-        });
-        return true;
-    }
+    final MenuItem searchMenuItem = menu.findItem( R.id.search);
+    final SearchView searchView   = (SearchView) searchMenuItem.getActionView();
+    searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+      @Override
+      public boolean onQueryTextSubmit(String query) {
+        Toast toast = Toast.makeText(getApplicationContext(), "Search For="+query, Toast.LENGTH_SHORT);
+        toast.show();
+        if( ! searchView.isIconified()) {
+          searchView.setIconified(true);
+        }
+        searchMenuItem.collapseActionView();
+        return false;
+      }
+      @Override
+      public boolean onQueryTextChange(String s) {
+        // UserFeedback.show( "SearchOnQueryTextChanged: " + s);
+        return false;
+      }
+    });
+    return true;
+  }
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
 
-        DictionaryAdapter da = new DictionaryAdapter(getApplicationContext());
+    DictionaryAdapter da = new DictionaryAdapter(getApplicationContext());
 
-        // mTextMessage = (TextView) findViewById(R.id.message);
-        // BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
-        // navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    // mTextMessage = (TextView) findViewById(R.id.message);
+    // BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+    // navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
 
-        rv = (RecyclerView)findViewById(R.id.rv);
-        rv.setHasFixedSize(true);
+    rv = (RecyclerView)findViewById(R.id.rv);
+    rv.setHasFixedSize(true);
 
-        // use a linear layout manager
-        mLayoutManager = new LinearLayoutManager(this);
-        rv.setLayoutManager(mLayoutManager);
+    // use a linear layout manager
+    mLayoutManager = new LinearLayoutManager(this);
+    rv.setLayoutManager(mLayoutManager);
 
 
-        mAdapter = new RecyclerViewAdapter(da.getRandomData());
-        rv.setAdapter(mAdapter);
+    mAdapter = new RecyclerViewAdapter(da.getRandomData());
+    rv.setAdapter(mAdapter);
 
-    }
+  }
 }
