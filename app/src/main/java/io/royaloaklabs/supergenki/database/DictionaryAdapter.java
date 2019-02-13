2ad519523de37ebe56372a64c4b7995911a68d2f
Eric <x16.tux@gmail.com>
2019-02-12 21:22:48 -0500

    -Handled case if theres NO DB -Added Hook for Search

--- app/src/main/java/io/royaloaklabs/supergenki/database/DictionaryAdapter.java
+++ app/src/main/java/io/royaloaklabs/supergenki/database/DictionaryAdapter.java
@@ -2,12 +2,9 @@ package io.royaloaklabs.supergenki.database;
 
 import android.content.Context;
 import android.database.Cursor;
-import android.database.SQLException;
 import android.database.sqlite.SQLiteDatabase;
-import android.util.Log;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 import io.royaloaklabs.supergenki.domain.JapaneseCard;
@@ -17,46 +14,53 @@ public class DictionaryAdapter {
 
     public DictionaryAdapter(Context context) {
         this.mDictHelper = new DictionaryHelper(context);
-        mDictHelper.connect();
+        if (!this.mDictHelper.hasNoDatabase() == true) {
+            mDictHelper.connect();
+        }
     }
 
 
     public List<JapaneseCard> getRandomData() {
-        SQLiteDatabase db = mDictHelper.getReadableDatabase();
+        if(this.mDictHelper.hasNoDatabase() != true) {
+            SQLiteDatabase db = mDictHelper.getReadableDatabase();
 
-        Cursor cursor = db.rawQuery("SELECT kanji, kana, gloss FROM einihongo ORDER BY RANDOM() LIMIT 20", null);
+            Cursor cursor = db.rawQuery("SELECT kanji, kana, gloss FROM einihongo ORDER BY RANDOM() LIMIT 20", null);
 
-        List<JapaneseCard> mCards = new ArrayList<JapaneseCard>();
+            List<JapaneseCard> mCards = new ArrayList<JapaneseCard>();
 
-        for(;cursor.moveToNext();) {
-            JapaneseCard jpCard = new JapaneseCard();
+            for (; cursor.moveToNext(); ) {
+                JapaneseCard jpCard = new JapaneseCard();
 
-            String kanjiCol = cursor.getString(cursor.getColumnIndex("kanji"));
-            String kanaCol = cursor.getString(cursor.getColumnIndex("kana"));
-            String senseCol = cursor.getString(cursor.getColumnIndex("gloss"));
+                String kanjiCol = cursor.getString(cursor.getColumnIndex("kanji"));
+                String kanaCol = cursor.getString(cursor.getColumnIndex("kana"));
+                String senseCol = cursor.getString(cursor.getColumnIndex("gloss"));
 
-            String[] kanji = kanjiCol.split(";");
-            String[] kana = kanaCol.split(";");
-            String[] sense = senseCol.split("\\|\\|");
+                String[] kanji = kanjiCol.split(";");
+                String[] kana = kanaCol.split(";");
+                String[] sense = senseCol.split("\\|\\|");
 
-            if(kanji.length != 0 && !kanji[0].isEmpty()) {
-                jpCard.setJapaneseText(kanji[0]);
-            } else {
-                jpCard.setJapaneseText(kana[0]);
+                if (kanji.length != 0 && !kanji[0].isEmpty()) {
+                    jpCard.setJapaneseText(kanji[0]);
+                } else {
+                    jpCard.setJapaneseText(kana[0]);
+                }
+
+                String[] senseArr = sense[0].split(";;");
+                StringBuilder sbSense = new StringBuilder();
+                sbSense.append(senseArr[0]);
+                for (int i = 1; i < senseArr.length; i++) {
+                    sbSense.append("; ");
+                    sbSense.append(senseArr[i]);
+                }
+                jpCard.setEnglishMeaning(sbSense.toString());
+
+                mCards.add(jpCard);
             }
 
-            String[] senseArr = sense[0].split(";;");
-            StringBuilder sbSense = new StringBuilder();
-            sbSense.append(senseArr[0]);
-            for(int i=1; i<senseArr.length; i++) {
-                sbSense.append("; ");
-                sbSense.append(senseArr[i]);
-            }
-            jpCard.setEnglishMeaning(sbSense.toString());
 
-            mCards.add(jpCard);
+            return mCards;
+        } else {
+            return new ArrayList<>();
         }
-
-        return mCards;
     }
 }
--- app/src/main/java/io/royaloaklabs/supergenki/domain/JapaneseEnglishEntry.java
+++
@@ -1,4 +0,0 @@
-package io.royaloaklabs.supergenki.domain;
-
-public class JapaneseEnglishEntry {
-}
