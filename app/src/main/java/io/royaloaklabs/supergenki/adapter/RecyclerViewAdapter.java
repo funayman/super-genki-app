63f58b530f524af0c3493ab0d85735d022e891cd
Dave Derderian <dave@drt.sh>
2019-02-15 10:57:01 +0900

    have RecyclerViewAdapter use new Entry domain to display information

--- app/src/main/java/io/royaloaklabs/supergenki/adapter/RecyclerViewAdapter.java
+++ app/src/main/java/io/royaloaklabs/supergenki/adapter/RecyclerViewAdapter.java
@@ -11,73 +11,79 @@ import androidx.recyclerview.widget.RecyclerView;
 import io.royaloaklabs.supergenki.MainActivity;
 import io.royaloaklabs.supergenki.R;
 import io.royaloaklabs.supergenki.activities.DetailedJapaneseActivity;
-import io.royaloaklabs.supergenki.domain.JapaneseCard;
+import io.royaloaklabs.supergenki.domain.Entry;
 
 import java.util.List;
 
 
 public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapter.JapaneseCardViewHolder> {
-    List<JapaneseCard> japaneseCards;
+  List<Entry> japaneseCards;
 
-    public RecyclerViewAdapter(List<JapaneseCard> japaneseCards){
-        this.japaneseCards = japaneseCards;
+  public RecyclerViewAdapter(List<Entry> japaneseCards) {
+    this.japaneseCards = japaneseCards;
+  }
+
+  @NonNull
+  @Override
+  public JapaneseCardViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+    View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.japanese_cardview, parent, false);
+    JapaneseCardViewHolder jcvh = new JapaneseCardViewHolder(v);
+    return jcvh;
+  }
+
+  @Override
+  public void onBindViewHolder(@NonNull JapaneseCardViewHolder holder, int position) {
+    if(japaneseCards.get(position).getKanji().isEmpty()) {
+      holder.kanjiText.setText(japaneseCards.get(position).getKana());
+      holder.kanaText.setText(R.string.EMPTY);
+    } else {
+      holder.kanjiText.setText(japaneseCards.get(position).getKanji());
+      holder.kanaText.setText(japaneseCards.get(position).getKana());
     }
 
-    @NonNull
-    @Override
-    public JapaneseCardViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.japanese_cardview, parent, false);
-        JapaneseCardViewHolder jcvh = new JapaneseCardViewHolder(v);
-        return jcvh;
-    }
+    holder.englishText.setText(japaneseCards.get(position).getSensesAsString());
 
-    @Override
-    public void onBindViewHolder(@NonNull JapaneseCardViewHolder holder, int position) {
-        holder.kanjiText.setText(japaneseCards.get(position).japaneseText);
-        holder.kanaText.setText(japaneseCards.get(position).kanaText);
-        holder.englishText.setText(japaneseCards.get(position).englishText);
-        holder.serializedForm = japaneseCards.get(position).japaneseText + "||"
-            + japaneseCards.get(position).englishText
-            + "||" + japaneseCards.get(position).kanaText;
-    }
+    holder.serializedForm = japaneseCards.get(position).getKanji()+ "||"
+        + japaneseCards.get(position).getSensesAsString()
+        + "||" + japaneseCards.get(position).getKana();
+  }
 
-    @NonNull
-    @Override
-    public int getItemCount() {
-        return japaneseCards.size();
-    }
+  @NonNull
+  @Override
+  public int getItemCount() {
+    return japaneseCards.size();
+  }
 
-    @Override
-    public void onAttachedToRecyclerView(RecyclerView recyclerView) {
-        super.onAttachedToRecyclerView(recyclerView);
-    }
+  @Override
+  public void onAttachedToRecyclerView(RecyclerView recyclerView) {
+    super.onAttachedToRecyclerView(recyclerView);
+  }
 
-    public static class JapaneseCardViewHolder extends RecyclerView.ViewHolder {
-        CardView cardView;
-        TextView kanjiText;
-        TextView kanaText;
-        TextView englishText;
-        String serializedForm;
+  public static class JapaneseCardViewHolder extends RecyclerView.ViewHolder {
+    CardView cardView;
+    TextView kanjiText;
+    TextView kanaText;
+    TextView englishText;
+    String serializedForm;
 
-        public JapaneseCardViewHolder(View itemView) {
-            super(itemView);
-            cardView    = (CardView)itemView.findViewById(R.id.cardView);
-            kanjiText   = (TextView)itemView.findViewById(R.id.japaneseText);
-            kanaText    = (TextView)itemView.findViewById(R.id.pronounceView);
-            englishText = (TextView)itemView.findViewById(R.id.translationView);
+    public JapaneseCardViewHolder(View itemView) {
+      super(itemView);
+      cardView = (CardView) itemView.findViewById(R.id.cardView);
+      kanjiText = (TextView) itemView.findViewById(R.id.japaneseText);
+      kanaText = (TextView) itemView.findViewById(R.id.pronounceView);
+      englishText = (TextView) itemView.findViewById(R.id.translationView);
 
-            itemView.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    System.out.println("Clicked");
-                    Intent intent = new Intent(view.getContext(), DetailedJapaneseActivity.class);
-                    intent.putExtra(MainActivity.MESSAGE, serializedForm);
-                    view.getContext().startActivity(intent);
+      itemView.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+          Intent intent = new Intent(view.getContext(), DetailedJapaneseActivity.class);
+          intent.putExtra(MainActivity.MESSAGE, serializedForm);
+          view.getContext().startActivity(intent);
 
-                }
-            });
         }
+      });
     }
+  }
 
 }
 
